b4408b0936e7c16dc0538193f54533e0fba99231
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-03-23 23:31:47 +0800

    Create Message_Queue_client.c

---
+++ Message_Queue_client.c
@@ -0,0 +1,30 @@
+#include "comm.h"
+
+int main()
+{
+	int msg_id = GetMsgQueue();
+	char buf[1024] = {0};
+
+	while(1)
+	{
+		printf("please enter: ");
+		fflush(stdout);
+
+		ssize_t s = read(0,buf,sizeof(buf));
+		if(s > 0)
+		{
+			buf[s-1] = 0;
+			SendMsg(msg_id,CLIENT_TYPE,buf);
+			printf("OK,pelase waiting..\n");
+		}
+
+		RecvMsg(msg_id,SERVER_TYPE,buf);
+		printf("server said: %s\n",buf);
+
+		if(strcasecmp("quit",buf))
+			break;
+	}
+
+	DestroyQueue(msg_id);
+	return 0;
+}
