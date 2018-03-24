37d5838904760496f3b328a050f688656556b53c
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-03-24 18:46:20 +0800

    Update wait.c

--- wait.c
+++ wait.c
@@ -1,51 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <string.h>
 
-extern char* environ
 int main()
 {
-        int i =0;
-        for(i = 0; environ[i] != NULL ;i++)
-            printf("%s\n",environ[i]);
-        if(execle("./hello","hello",NULL,environ ) == -1)
-            perror("exec"),exit(1);
+	pid_t pid;
+
+	if((pid = fork()) < 0)
+		perror("fork"),exit(1);
+
+	if(pid == 0)
+	{
+		int i = 0;
+		while(1)
+		{
+			printf("$$$\n");
+			sleep(1);
+			//i++;
+			//if(i == 3)
+			//	break;
+		}
+		//exit(2);
+	}
+	else
+	{
+		int st;
+		pid_t r;
+		if((r = wait(&st)) < 0)
+			perror("wait");
+		printf("pid= %d,r= %d ,ret= %d",pid,r,(st>>8)&0xff);
+		if(WIFEXITED(st))
+			printf("exit code= %d\n",WEXITSTATUS(st));
+		else if(WIFSIGNALED(st))
+			printf("signal code = %d\n",WTERMSIG(st));
+	}
+
+	return 0;
 }
-
- #include <stdio.h>
- #include <stdlib.h>
- #include <unistd.h>
- #include <sys/wait.h>
- #include <string.h>
- 
-  int main()
-  {
-      pid_t pid;
-
-     if((pid = fork()) < 0)
-         perror("fork"),exit(1);
-
-     if(pid == 0)
-     {
-         int i = 0;
-         while(1)
-         {
-             printf("$$$\n");
-             sleep(1);
-             i++;
-             if(i == 3)
-                 break;
-        }
-         exit(2);
-     }
-    else
-    {
-         int st;
-         pid_t r;
-         if((r = wait(&st)) < 0)
-            perror("wait");
-         printf("pid= %d,r= %d ,ret= %d",pid,r,(st>>8)&0xff);
-   }
-    return 0;
- }
