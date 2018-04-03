069b8738a68ced9d9db1dd30a5373131b643e13b
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-04-03 20:25:01 +0800

    Create Sem_comm.c

---
+++ Sem_comm.c
@@ -0,0 +1,68 @@
+#include "comm.h"
+
+static int CreatSem(int nums,int flags)
+{
+	key_t key = ftok(PATHNAME,PROJ_ID);
+	if(key < 0)
+		ERR_EXIT("ftok");
+	
+	int semid = semget(key,nums,flags);
+	if(semid < 0)
+		ERR_EXIT("semget");
+}
+
+int creatSemSet(int nums)
+{
+	return CreatSem(nums,IPC_CREAT|IPC_EXCL|06666);
+}
+
+int initSem(int semid,int num,initval)
+{
+	union semun un;
+	un.val = initval;
+	if(semctl(semid,num,SETVAL,un) < 0)
+	{
+		ERR_EXIT("semctl");
+		return -1;
+	}
+	return 0;
+}
+
+int getSemSet(int nums)
+{
+	return CreatSem(nums,IPC_CREAT|06666);
+}
+
+static int PV(int semid,int num,int op)
+{
+	struct sembuf _sf;
+	_sf.sem_num = num;
+	_sf.sem_op = op;
+	_sf.sem_flg = 0;//NO_WAIT
+	if(semop(semid,&_sf,1) < 0)
+	{
+		ERR_EXIT("semop");
+		return -1;
+	}
+	return 0;
+}
+
+int P(int semid,int num)
+{
+	return PV(semid,num,-1);	
+}
+
+int V(int semid,int num)
+{
+	return PV(semid,num,1);
+}
+
+int destroySetSet(int semid)
+{
+	if(semctl(semid, 0, IPC_RMID)<0)
+	{
+		ERR_EXIT("destroy");
+		return -1;
+	}
+	return 0;
+}
