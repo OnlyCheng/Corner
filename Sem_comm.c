7d1438643d823a7409262e92486ebc96c882bc40
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-04-05 21:30:22 +0800

    Update Sem_comm.c

--- Sem_comm.c
+++ Sem_comm.c
@@ -9,28 +9,26 @@ static int CreatSem(int nums,int flags)
 	int semid = semget(key,nums,flags);
 	if(semid < 0)
 		ERR_EXIT("semget");
+	return semid;
 }
 
 int creatSemSet(int nums)
 {
-	return CreatSem(nums,IPC_CREAT|IPC_EXCL|06666);
+	return CreatSem(nums,IPC_CREAT|IPC_EXCL|0666);
 }
 
-int initSem(int semid,int num,initval)
+int initSem(int semid,int num,int initval)
 {
 	union semun un;
 	un.val = initval;
 	if(semctl(semid,num,SETVAL,un) < 0)
-	{
 		ERR_EXIT("semctl");
-		return -1;
-	}
 	return 0;
 }
 
 int getSemSet(int nums)
 {
-	return CreatSem(nums,IPC_CREAT|06666);
+	return CreatSem(nums,IPC_CREAT);
 }
 
 static int PV(int semid,int num,int op)
@@ -40,10 +38,7 @@ static int PV(int semid,int num,int op)
 	_sf.sem_op = op;
 	_sf.sem_flg = 0;//NO_WAIT
 	if(semop(semid,&_sf,1) < 0)
-	{
 		ERR_EXIT("semop");
-		return -1;
-	}
 	return 0;
 }
 
@@ -60,9 +55,6 @@ int V(int semid,int num)
 int destroySetSet(int semid)
 {
 	if(semctl(semid, 0, IPC_RMID)<0)
-	{
 		ERR_EXIT("destroy");
-		return -1;
-	}
 	return 0;
 }
