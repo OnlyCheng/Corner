37cec5f1d9c0082bbb412c1769cd96e3cf7b65d3
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-04-19 16:20:45 +0800

    Update mydaemon.c

--- mydaemon.c
+++ mydaemon.c
@@ -27,8 +27,7 @@ void MyDaemon()
 	pid = fork();
 	if(pid < 0)//调用fork失败
 		ERR_EXIT("fork");
-
-	if(pid > 0)//父进程
+	else if(pid > 0)//父进程
 		exit(0);
 	
 	//调用setsid函数
@@ -45,39 +44,37 @@ void MyDaemon()
 	//再次fork（）
 	if((pid = fork()) < 0)
 		ERR_EXIT("fork");
-	else if(fork > 0)//父进程
+	else if(pid > 0)//父进程
 		exit(0);
 	
 	//更改当前目录为根目录
 	if(chdir("/") < 0)
 	{
 		printf("change child dir error\n");
-		return;
+		exit(EXIT_FAILURE);
 	}
 
 	//关闭不需要的文件描述符
-	//
-	//close(0);
-	//close(1);
-	//close(2);
-	
+/*
+	close(0);
+	close(1);
+	close(2);
+*/
 	//重定向到/dev/null
 	fd0 = open("/dev/null",O_RDWR);
 	dup2(fd0,0);
 	dup2(fd0,1);
 	dup2(fd0,2);
-
 	//已成功创建一个守护进程，为了方便验证，让守护进程为我们提供一项服务
 	while(1)
 	{
-		fd0 = open("daemon.txt", O_WRONLY|O_CREAT|O_APPEND, 0644);
+		fd0 = open("mydaemon.txt", O_WRONLY|O_CREAT|O_APPEND, 0644);
 		if(fd0 == -1)
 			ERR_EXIT("open");
 		char *buf = "I'm offering you a service\n";
 		write(fd0, buf, strlen(buf));
 		close(fd0);
 		sleep(60);
-	
 	}
 }
 
