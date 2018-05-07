d7f5c586561ad5ded3cc245f529464bcea315b6f
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-05-07 20:30:40 +0800

    Update TcpServer.c

--- TcpServer.c
+++ TcpServer.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <socket.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define BACKLOG 10
 
-int mian(int argc,char* argv[])
+int main(int argc,char* argv[])
 {
 	if(argc < 3)
 	{
@@ -16,7 +18,7 @@ int mian(int argc,char* argv[])
 	}
 	int listen_sock = socket(AF_INET, SOCK_STREAM,0);
 	if(listen_sock < 0)
-		printf("create listen_ocket error,errno is:%d errstring is: %s\n",errno,strerron(errno));
+		printf("create listen_socket error,errno is:%d errstring is: %s\n",errno,strerror(errno));
 
 	struct sockaddr_in server_socket;
 	struct sockaddr_in client_socket;
@@ -25,7 +27,7 @@ int mian(int argc,char* argv[])
 	server_socket.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_socket.sin_port = htons(atoi(argv[2]));
 
-	if(bind(listen_sock,(struct sockaddr*)&server_socket,sizeof(server_socket)) < 0);
+	if(bind(listen_sock,(struct sockaddr*)&server_socket,sizeof(server_socket)) < 0)
 	{
 		printf("bind failed,errno is:%d ,errstring is:%s\n",errno,strerror(errno));
 		close(listen_sock);
@@ -51,27 +53,38 @@ int mian(int argc,char* argv[])
 			close(listen_sock);
 			return 4;
 		}
-	}
-
-	char buf[INET_ADDRSTRLEN];
-	buf[0] = 0;
-	inet_ntop(AF_INET,&client_socket.sin_addr.s_addr,buf,sizeof(buf));
-	printf("get a connect,ip is: %s port is:%d\n",buf,ntohs(client_socket.sin_port));
-	while(1)
-	{
-		char s[1024] = 0;
-		ssize_t n = read(client_sock,s,sizeof(s)-1);
-		if(s > 0)
+		char buf_ip[INET_ADDRSTRLEN];
+		buf_ip[0] = 0;
+		inet_ntop(AF_INET,&client_socket.sin_addr.s_addr,buf_ip,sizeof(buf_ip));
+		printf("get a connect,ip is: %s port is:%d\n",buf_ip,ntohs(client_socket.sin_port));
+		while(1)
 		{
-			printf("client ## %s\n",buf);
-			printf("server $$");
-			memset(s,'\0',sizeof(s));
-			read(0,s,sizeof(s)-1);
-			write(client_sock,s,strlen(s)-1);
-			printf("please wait--\n");
-		}
+			char buf[1024];
+			buf[0] = 0;
+			ssize_t s = read(client_sock,buf,sizeof(buf)-1);
+			if(s > 0)
+			{
+				buf[s-1] = 0;
+				if(strcmp(buf,"quit") == 0)
+				{
+					printf("client requet quit\n");
+					return 0;
+				}
 
+				printf("client ## %s\n",buf);
+				printf("server $$ ");
+				fflush(stdout);
+				memset(buf,'\0',sizeof(buf));
+				s = read(0,buf,sizeof(buf)-1);
+				if(s > 0)
+				{
+					buf[s-1] = 0;
+					write(client_sock,buf,sizeof(buf)-1);	
+				}
+			}
+		}
 	}
 	close(listen_sock);
 	return 0;
 }
+
