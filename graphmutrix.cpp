0183e417a3e5bb3e70aca17ba40b8150ec539df4
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-05-08 18:02:50 +0800

    Update graphmutrix.cpp

--- graphmutrix.cpp
+++ graphmutrix.cpp
@@ -1,96 +1,105 @@
+#pragma once
+
 #include <iostream>
 using namespace std;
+#include <vector>
+#include <assert.h>
 
-template <class V,class W,bool IsDirect = false >
-class Graph 
-{ 
-public: 
-Graph(V* array, size_t size) 
-	: _v(array, array+size) 
-{ 
-	_edges.resize(size); 
-	for(size_t i = 0; i < size; ++i) 
-		_edges[i].resize(size); 
-} 
-
-// 获取顶点元素在其数组中的下标 
-size_t GetIndexOfV(const V& v)
+template<class V, bool IsDirect = false>
+class Graph
 {
-	for(size_t i = 0;i < _v.size();i++)
+public:
+	Graph(V* arr, int size)
+		:_v(arr, arr + size)
 	{
-		if(v == _v[i])
-			return i;
-	}
-	cout<<"顶点非法"<<endl;
-}
-
-// 获取顶点的度 
-size_t GetDevOfV(const V& v)
-{
-	size_t count = 0;
-	size_t size = _v.size();
-	size_t index = GetIndexOfV(v);
-	//出度
-	for(size_t i = 0; i<size;i++)
-	{
-		if(_edges[index][i] > 0)
-			count++;
-	}
-	if(IsDirect)
-	{	
-		//入度
-		for(size_t i = 0;i<size; i++)
+		_edges.resize(size);
+		for (int i = 0; i < size; i++)
 		{
-			if(_edges[i][index] > 0)
-				count++;
+			_edges[i].resize(size);
 		}
 	}
-	return count;
-}
 
-// 添加顶点v1与v2对应的权值为weight的边 
-void AddEdge(const V& v1, const V& v2, const W& weight)
-{
-	int index1 = GetIndexOfV(v1);
-	int index2 = GetIndexOfV(v2);
-	_edges[index1][index2] = weight;
-	if(!IsDirect)
+	bool Add(const V& v1, const V& v2, int weight)
 	{
-		_edges[index2][index1] = weight;
+		int i1 = GetIndexOfV(v1);
+		int i2 = GetIndexOfV(v2);
+		assert(i1 >= 0 && i2 >= 0);
+
+		_edges[i1][i2] = weight;
+		if (!IsDirect)
+			_edges[i2][i1] = weight;
+		return true;
 	}
-}
 
-// 图的打印 
-void PrintGraph()
-{
-	size_t size = _v.size();
-	for(size_t i = 0;i<size; i++)
+	int GetDevOfV(const V& v)
 	{
-		for(size_t j = 0; i<size; j++)
+		int size = _v.size();
+		if (size == 0)
+			return 0;
+		int count = 0;
+		int index = GetIndexOfV(v);
+		if (index >= 0)
 		{
-			cout<<_edges[i][j]<<"  ";
+			for (int i = 0; i<size; i++)
+			{
+				if (_edges[index][i] != 0)
+					count++;
+			}
+			if (IsDirect)
+			{
+				for (int i = 0; i<size; i++)
+				{
+					if (_edges[i][index] != 0)
+						count++;
+				}
+			}
 		}
-		cout<<endl;
+		return count;
 	}
-	cout<<endl;
-}
 
-private: 
-	vector<V> _v; 
-	vector< vector<W> > _edges; 
-}; 
+	void PrintGraph()
+	{
+		int size = _v.size();
+		if (size == 0)
+			return;
+		for (int i = 0; i<size; i++)
+		{
+			for (int j = 0; j < size; j++)
+				printf("%-2d  ", _edges[i][j]);
+			cout << endl;
+		}
+	}
+private:
+	int GetIndexOfV(const V& v)
+	{
+		int size = _v.size();
+		if (size == 0)
+			return -1;
+		for (int i = 0; i<size; i++)
+		{
+			if (v == _v[i])
+				return i;
+		}
+		return -1;
+	}
+private:
+	vector<V> _v;
+	vector<vector<int> > _edges;
+};
 
-void testmutrix()
+void TestMutrix()
 {
-	char buf[] = {'A','B','C','D','E'};
-	Graph<char,int> g(buf,sizeof(buf)/sizeof(buf[0]));
-	g.AddEdge('A','B',25);
-	g.AddEdge('B','E',20);
-	g.AddEdge('C','D',10);
-	g.AddEdge('C','B',40);
-	g.AddEdge('E','A',30);
+	char arr[] = { 'A', 'B', 'C', 'D', 'E', 'F' };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	Graph<char> g(arr,size);
+	g.Add('A', 'B', 10);
+	g.Add('A', 'D', 20);
+	g.Add('C', 'B', 40);
+	g.Add('E', 'F', 10);
+	g.Add('F', 'D', 60);
+	g.Add('F', 'B', 50);
 
+	int dev = g.GetDevOfV('A');
+	cout << "The dev of A is: " << dev << endl;
 	g.PrintGraph();
-	size_t dev = g.GetDevOfV('A');
-	cout<< "A的度为："<<dev<<endl;
 }
