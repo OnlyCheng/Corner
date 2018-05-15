508606f4c516ea60d4e23f1bbb24950488feae45
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-05-15 23:07:29 +0800

    Create BinaryTreeMirror.cpp

---
+++ BinaryTreeMirror.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if(pRoot == NULL)
+            return;
+        TreeNode* temp;
+        temp = pRoot->left;
+        pRoot->left = pRoot->right;
+        pRoot->right = temp;
+        
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);
+    }
+};
