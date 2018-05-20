7e6d7680816a6941758060c5dfe4097e1abacee6
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-05-21 00:04:18 +0800

    Update printMatrix.cpp

--- printMatrix.cpp
+++ printMatrix.cpp
@@ -5,24 +5,36 @@ public:
         int col = matrix.size();
         int row = matrix[0].size();
         vector<int> res;
-        if (col == 0 || row == 0)
+        if (col <= 0 || row <= 0)
             return res;
-        for (int i = 0; i < (col + 1) / 2; i++)
+        for (int i = 0; res.size()<col*row && i<row; i++)
         {
             int j = i;
-            for (; j < col - i - 1; j++)
-                res.push_back(matrix[i][j]);
+            if(j < col-1)
+            {
+                for (; j < col - i - 1; j++)
+                    res.push_back(matrix[i][j]);
+            }
             int k = i;
-            for (; k<row - i - 1; k++)
-                res.push_back(matrix[k][j]);
+            if(k < row)
+            {
+                for (; k<row - i - 1; k++)
+                    res.push_back(matrix[k][j]);
+            }
             
             int m = j;
-            for (; m > i; m--)
-                res.push_back(matrix[k][m]);
+            if(m < col)
+            {
+                for (; m > i; m--)
+                    res.push_back(matrix[k][m]);
+            }
             
             int n = k;
-            for (; n > i; n--)
-                res.push_back(matrix[n][m]);
+            if(n < row)
+            {
+                for (; n > i; n--)
+                    res.push_back(matrix[n][m]);
+            }
         }
         return res;
     }
