fc0f19ebf09817a009bebfa20c1847f1728fb077
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-05-22 18:50:27 +0800

    Update printMatrix.cpp

--- printMatrix.cpp
+++ printMatrix.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    vector<int> printMatrix(vector<vector<int> > matrix)
-    {
-        int col = matrix.size();
-        int row = matrix[0].size();
+    vector<int> printMatrix(vector<vector<int> > matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
         vector<int> res;
         if (col <= 0 || row <= 0)
             return res;
-        for (int i = 0; res.size()<col*row && i<row; i++)
+        int left = 0;
+        int right = col-1;
+        int up = 0;
+        int down = row-1;
+        while(left<=right && up<=down)
         {
-            int j = i;
-            if(j < col-1)
-            {
-                for (; j < col - i - 1; j++)
-                    res.push_back(matrix[i][j]);
-            }
-            int k = i;
-            if(k < row)
-            {
-                for (; k<row - i - 1; k++)
-                    res.push_back(matrix[k][j]);
-            }
-            
-            int m = j;
-            if(m < col)
-            {
-                for (; m > i; m--)
-                    res.push_back(matrix[k][m]);
-            }
-            
-            int n = k;
-            if(n < row)
-            {
-                for (; n > i; n--)
-                    res.push_back(matrix[n][m]);
-            }
+            //从左到右遍历
+            for(int j = left;j<=right; j++)
+                res.push_back(matrix[up][j]);
+            //从上到下遍历
+            if(up<down)
+                for(int k = up+1; k<=down; k++)
+                    res.push_back(matrix[k][right]);
+            //从右到左遍历
+            if(up<down && left<right)
+                for(int j = right-1; j>=left; j--)
+                    res.push_back(matrix[down][j]);
+            //从下到上遍历
+            if(up<down && left<right)
+                for(int k = down-1; k>up; k--)
+                    res.push_back(matrix[k][left]);
+            left++;right--;
+            up++;down--;
         }
         return res;
     }
