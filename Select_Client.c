0ddf742f30a2f0c17a3c54505848e3b1fa337e85
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-06-03 20:27:39 +0800

    Update Select_Client.c

--- Select_Client.c
+++ Select_Client.c
@@ -8,42 +8,39 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define BACKLOG 10
-
 int main(int argc,char* argv[])
 {
-	if(argc != 2)
+	if(argc != 3)
 	{
-		printf("./tcpServer [addr:] [port:]\n");
+		printf("./Server [addr:] [port:]\n");
 		return 1;
 	}
 	int sock = socket(AF_INET, SOCK_STREAM,0);
 	if(sock < 0)
 	{
-		printf("create sock error,errno is:%d errstring is: %s\n",errno,strerror(errno));
+		perror("socket");
 		return 2;
 	}
 
 	struct sockaddr_in server_socket;
-	bzero(&server_socket,sizeof(server_socket));
 	server_socket.sin_family = AF_INET;
-	inet_pton(AF_INET,INADDR_ANY,&server_socket.sin_addr);
-	server_socket.sin_port = htons(atoi(argv[1]));
+	server_socket.sin_addr.s_addr=inet_addr(argv[1]);
+	server_socket.sin_port = htons(atoi(argv[2]));
 
+	printf("start connet..\n ");
 	if(connect(sock,(struct sockaddr*)&server_socket,sizeof(server_socket)) < 0)
 	{
-		printf("connect failed, errno is %d error string is %s\n",errno,strerror(errno));
+		perror("connect");
 		close(sock);
 		return 3;
 	}
 
 	printf("connect success..\n ");
-	
 	char buf[1024];
 	buf[0] = 0;
 	while(1)
 	{
-		printf("client:## ");
+		printf("please enter>: ");
 		fflush(stdout);
 		ssize_t s = read(0,buf,sizeof(buf)-1);
 		if(s > 0)
