0eabf8ec8901240253edd88250a25101b7bf8687
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-07-11 21:00:57 +0800

    Create permuteUnique.cpp

---
+++ permuteUnique.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        vector<vector<int> > res;
+        int size = num.size();
+        if(size  == 0)
+            return res;
+        
+        vector<int> flag(size,-1);
+        vector<int> temp(size);
+        _permute(res,flag,num,temp,0);
+        return res;
+    }
+    void _permute(vector<vector<int> >& res, vector<int> &flag,vector<int> &num,vector<int>temp,int n)
+    {
+        int size = num.size();
+        if(n == size)
+        {
+            int k = 0;
+            for(;k<res.size();k++)
+            {
+                if(res[k] == temp)
+                    break;
+            }
+            if(k == res.size())
+                res.push_back(temp);
+        }
+            
+        else
+        {
+            for(int i = 0; i<size; i++)
+            {
+                if(flag[i] == -1)
+                {
+                    temp[n]=num[i];
+                    flag[i] = 0;
+                    _permute(res,flag,num,temp,n+1);
+                    flag[i] = -1;
+                    continue;
+                }
+            }
+        }
+    }
+};
