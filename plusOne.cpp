fc67a6a2e2574d4747882fa4b3990e6aadd637d7
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-07-12 15:45:43 +0800

    Create plusOne.cpp

---
+++ plusOne.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int size = digits.size();
+        vector<int> res(size);
+        if(size == 0)
+            res.push_back(1);
+        else
+        {
+            int flag = 1;
+            for(int i = size-1; i >= 0; i--)
+            {
+                int cur = digits[i]+flag;
+                res[i] = cur%10;
+                if(cur <= 9)
+                    flag = 0;
+                else
+                    flag = 1;
+            }
+            if(flag)
+                res.insert(res.begin(),1);
+        }
+        return res;
+    }
+};
