f2e7d25e6578e9f1fa9fd638cc2015bcaa63ffe9
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-07-12 16:00:31 +0800

    Update plusOne.cpp

--- plusOne.cpp
+++ plusOne.cpp
@@ -12,6 +12,29 @@ public:
             {
                 int cur = digits[i]+flag;
                 res[i] = cur%10;
+                flag = cur/10;
+            }
+            if(flag)
+                res.insert(res.begin(),1);
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int size = digits.size();
+        vector<int> res(size);
+        if(size == 0)
+            res.push_back(1);
+        else
+        {
+            int flag = 1;
+            for(int i = size-1; i >= 0; i--)
+            {
+                int cur = digits[i]+flag;
+                res[i] = cur%10;
                 if(cur <= 9)
                     flag = 0;
                 else
