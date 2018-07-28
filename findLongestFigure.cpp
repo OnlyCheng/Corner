dd3b0fb587ba54d738d5073c30a3927d90c3b670
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-07-28 23:38:16 +0800

    Update findLongestFigure.cpp

--- findLongestFigure.cpp
+++ findLongestFigure.cpp
@@ -35,3 +35,37 @@ int main()
     }
     return 0;
 }
+
+//上边的方法OJ能过66.7%，去掉循环就能过了
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+    string s;
+    cin>>s;
+    //while(cin>>s)
+    //{
+        int size = s.size();
+        if(size == 0)
+        {
+            cout<<""<<endl;
+            return 0;
+        }
+        string res;
+        string cur;
+        for(int i = 0; i<size;i++)
+        {
+            if(s[i]>='0' && s[i]<='9')
+                cur+=s[i];
+            else
+                cur = "";
+            if(cur.size()>res.size())
+                res = cur;
+        }
+        cout<<res<<endl;
+    //}
+    
+    return 0;
+}
