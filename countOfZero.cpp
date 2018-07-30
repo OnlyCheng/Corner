7925b8ab3e6ef6e1eafe3700ffd5d950a967a2d3
OnlyCheng <35389599+OnlyCheng@users.noreply.github.com>
2018-07-30 21:21:41 +0800

    Update countOfZero.cpp

--- countOfZero.cpp
+++ countOfZero.cpp
@@ -13,3 +13,29 @@ int main()
     cout<<count<<endl;
     return 0;
 }
+
+//方法2
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int count = 0;
+    for(int i = 5;i<=n;i = i+5)
+    {
+        int j = i;
+        while(j)
+        {
+            if(j%5 ==0)
+                count++;
+            else
+                break;
+            j /= 5;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
